90b6631817cfbf477656a1d6709515a39858c79a
Unity <guilhermtrout@gmail.com>
2025-03-10 20:12:04 -0300

    feat: implement test

--- test/myball_test.dart
+++ test/myball_test.dart
@@ -1,38 +1,8 @@
-import 'package:brickbreaker/ball.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Test MyBall widget rendering', (WidgetTester tester) async {
-    // create widget with ballX & ballY defined
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: MyBall(ballX: 0.5, ballY: 0.5),
-        ),
-      ),
-    );
-
-    // Checks if the widget is rendered
-    expect(find.byType(MyBall), findsOneWidget);
-
-    // Checks if the container with the ball is present
-    final ballFinder = find.byType(Container);
-    expect(
-        ballFinder,
-        findsNWidgets(
-            2)); // You should find 2 containers (the MyBall one and the internal one)
-
-    // Tests if the container alignment is correct based on the ballX and ballY values
-    final containerWidget =
-        tester.widget<Container>(find.byType(Container).last);
-    final alignment = containerWidget.alignment;
-
-    // Checks if the alignment is of type Alignment (which has properties x and y)
-    expect(alignment, isA<Alignment>());
-    final alignmentValue = alignment as Alignment;
-
-    expect(alignmentValue.x, 0.5);
-    expect(alignmentValue.y, 0.5);
-  });
+  testWidgets(
+      'Testa se a bola se move para cima', (WidgetTester tester) async {
+        final ball = find.byKey(Key('ball'));
+      });
 }
